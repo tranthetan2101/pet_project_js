b65b4f3394bdce18b9383823e3657c55c21f8226
tranthetan2101 <tantran21012000@gmail.com>
2022-04-20 16:52:20 +0700

    first Added my project

---
+++ frontend/src/api/axiosClient.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+    headers: {
+        'content-type': 'application/json',
+    },
+    paramsSerializer: params => queryString.stringify(params),
+});
+
+axiosClient.interceptors.request.use(async (config) => {
+    // Handle token here ...
+    return config;
+});
+
+axiosClient.interceptors.response.use((response) => {
+    if (response && response.data) {
+    return response.data;
+}
+    return response;
+}, (error) => {
+    throw error;
+});
+
+export default axiosClient;
